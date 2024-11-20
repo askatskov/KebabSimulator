675d3c31a36d8f58e3bbd165bfd0e56f8fb359a8
opilane <opilane@HP-CZC321705F>
2024-11-20 15:21:08 +0200

    Added AccountsServices

---
+++ Kebab Simulator.ApplicationServices/Services/AccountsServices.cs
@@ -0,0 +1,26 @@
+﻿using Kebab_Simulator.Core.Domain;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kebab_Simulator.ApplicationServices.Services
+{
+    public class AccountsServices : IAccountsServices
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountsServices
+            (
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
