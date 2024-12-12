e28be34c447261e135f31ac132c19036412bcaf5
awesa <awesa@DESKTOP-L09ETCB>
2024-12-12 13:38:33 +0200

    master

--- Kebab Simulator/Models/Emails/EmailViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kebab_Simulator.Models.Emails
-{
-    public class EmailViewModel
-    {
-        public string To { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
-    }
-}
